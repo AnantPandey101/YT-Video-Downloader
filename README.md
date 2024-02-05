d860d943f017234ba90081f77361d3ef32a1ef20
AnantPandey101 <anantpandey3010@gmail.com>
2024-02-06 00:46:55 +0530

    Version 1.0.1 - GUI Fixing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # YT-Video-Downloader
+<br>
+This application can be used to download videos and audios from YouTube.
--- YT_Video_Downloader.py
+++ YT_Video_Downloader.py
@@ -5,7 +5,7 @@ import customtkinter
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-
+# function defining download procedures
 def Download_Initiation(option):
     try:
         yt_link = YouTube(link_dialogue_box.get(), on_progress_callback = Download_Progress)
@@ -23,22 +23,28 @@ def Download_Initiation(option):
     except:
         '''final_label.configure(text="Download Error!")'''
 
+
+# function for Download Progress Bar
 def Download_Progress():
     pass
 
-
+# GUI for the app
 root = customtkinter.CTk()
 
-root.title("YT Downloader 1.0.0")
-root.geometry('1080*480')
+root.title("YT Downloader 1.0.1")
+root.minsize(550,220)
+root.maxsize(550,220)
 
-link_dialogue_box = customtkinter.CTkEntry(root)
+yt_label = customtkinter.CTkLabel(root, text= "YouTube U/A Downloader", font= ("Comic Sans", 20))
+
+link_dialogue_box = customtkinter.CTkEntry(root, width= 500, placeholder_text="Enter URL")
 
 video_button = customtkinter.CTkButton(root, text= 'Video', command=lambda: Download_Initiation('Video'))
 audio_button = customtkinter.CTkButton(root, text= 'Audio', command=lambda: Download_Initiation('Audio'))
 
-link_dialogue_box.pack()
-video_button.pack()
-audio_button.pack()
+yt_label.grid(row= 0, column= 0, columnspan=3, pady= 20)
+link_dialogue_box.grid(row= 1, column= 0, columnspan= 3,padx=30, pady=20)
+video_button.grid(row= 2, column = 0, padx= 10, pady= 20)
+audio_button.grid(row= 2, column = 2, padx= 10, pady= 20)
 
 root.mainloop()
